84fa0c4dbfc0bb3a2a0bb7dfd50658ebecfa20be
suyeon <paksy0428@gmail.com>
2025-06-22 03:56:58 +0900

    design: 팀 대시보드 주간 활약 선수 디자인

---
+++ src/components/Team/Player/team-player.tsx
@@ -0,0 +1,38 @@
+import CommonButton from '../CommonButton/common-button';
+import FeaturedPlayerCard from './featured-player-card';
+
+const featuredPlayers = [
+  {
+    title: '인기 타자',
+    position: '3루수',
+    name: '김도영',
+    statLabel: '타율',
+    statValue: '0.500',
+    className: 'w-1/2',
+  },
+  {
+    title: '인기 타자',
+    position: '3루수',
+    name: '김도영',
+    statLabel: '타율',
+    statValue: '0.500',
+    className: 'w-1/2',
+  },
+];
+
+const TeamPlayer = () => {
+  return (
+    <div className="grid place-items-center">
+      <div className="text-2xl font-semibold mt-4 mb-4">주간 활약 선수</div>
+      <CommonButton variant="outlined">선수 명단 보기</CommonButton>
+      <div className="grid grid-cols-2 gap-4 max-w-3xl w-full">
+        {featuredPlayers.map((record, index) => (
+          <FeaturedPlayerCard key={index} {...record} />
+        ))}
+      </div>
+      <hr className="border-t border-gray-100 my-8" />
+    </div>
+  );
+};
+
+export default TeamPlayer;
--- src/pages/team-dashboard-page.tsx
+++ src/pages/team-dashboard-page.tsx
@@ -2,13 +2,15 @@ import TeamBanner from '../components/Team/Banner/team-banner';
 import TeamInfoButton from '../components/Team/Banner/team-info-button';
 import MatchRecord from '../components/Team/Match/match-record';
 import MatchSchedule from '../components/Team/Match/match-schedule';
+import TeamPlayer from '../components/Team/Player/team-player';
 
 const TeamDashboardPage = () => {
   return (
     <div>
-      <TeamBanner teamKey="lg" />
+      <TeamBanner teamKey="kia" />
       <MatchSchedule />
       <MatchRecord />
+      <TeamPlayer />
     </div>
   );
 };
