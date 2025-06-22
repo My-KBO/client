09d041c2542f480b8541b8c320ab3c6203fd3a28
suyeon <paksy0428@gmail.com>
2025-06-22 19:44:06 +0900

    design: 팀 대시보드 팬들의 응원 파트 디자인

--- src/pages/team-dashboard-page.tsx
+++ src/pages/team-dashboard-page.tsx
@@ -1,5 +1,6 @@
 import TeamBanner from '../components/Team/Banner/team-banner';
 import TeamInfoButton from '../components/Team/Banner/team-info-button';
+import FanComment from '../components/Team/Comment/fan-comment';
 import MatchRecord from '../components/Team/Match/match-record';
 import MatchSchedule from '../components/Team/Match/match-schedule';
 import TeamPlayer from '../components/Team/Player/team-player';
@@ -11,6 +12,7 @@ const TeamDashboardPage = () => {
       <MatchSchedule />
       <MatchRecord />
       <TeamPlayer />
+      <FanComment />
     </div>
   );
 };
