1978b33d702b66cb46e0f79595e9705014dbca31
suyeon <paksy0428@gmail.com>
2025-06-23 01:20:21 +0900

    design: 팀 대시보드 최근 뉴스 디자인

--- src/pages/team-dashboard-page.tsx
+++ src/pages/team-dashboard-page.tsx
@@ -1,10 +1,10 @@
 import TeamBanner from '../components/Team/Banner/team-banner';
-import TeamInfoButton from '../components/Team/Banner/team-info-button';
 import FanComment from '../components/Team/Comment/fan-comment';
 import Highlight from '../components/Team/Highlight/highlight';
 import MatchRecord from '../components/Team/Match/match-record';
 import MatchSchedule from '../components/Team/Match/match-schedule';
 import TeamPlayer from '../components/Team/Player/team-player';
+import TeamNews from '../components/Team/TeamNews/team-news';
 
 const TeamDashboardPage = () => {
   return (
@@ -15,6 +15,7 @@ const TeamDashboardPage = () => {
       <TeamPlayer />
       <FanComment />
       <Highlight teamKey="kia" />
+      <TeamNews />
     </div>
   );
 };
