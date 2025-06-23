e4e11583196a4cb7ed45cb9128447843f388cda1
Kang Ye Ram <kyj5730@gmail.com>
2025-06-23 21:50:32 +0900

    design: tailwind.config.js 파일에 폰트 및 색상 디자인설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Pretendard', 'sans-serif'],
+      },
+      colors: {
+        white: '#fff',
+        gray50: '#f7f8f9',
+        gray100: '#e8ebed',
+        gray200: '#c9cdd2',
+        gray400: '#9ea4aa',
+        gray500: '#72797f',
+        gray600: '#454c53',
+        gray800: '#26282b',
+        gray900: '#1b1d1f',
+        'kbo-blue': '#002561',
+        'kbo-lightblue': '#00AEEf',
+        'kbo-red': '#ed1c24',
+      },
+    },
   },
   plugins: [],
 };
